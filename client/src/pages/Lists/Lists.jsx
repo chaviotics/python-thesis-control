7a0cab608f854697b0574701b3010dcd7bcbb37c
Chav Sumalinog <chaviotics@gmail.com>
2023-05-05 16:00:34 +0800

    Finalized Lists page

--- client/src/pages/Lists/Lists.jsx
+++ client/src/pages/Lists/Lists.jsx
@@ -48,7 +48,38 @@ function MainContent() {
 
         {/* List Mutable Example */}
 
-        {/*  */}
+        <Typography mt="2rem" mb="1rem">
+          Lists are mutable, which means that you can change the items in a list
+          after it has been created.
+        </Typography>
+
+        <CodeMutableExample />
+
+        {/* append() */}
+        <Typography mt="2rem" mb="1rem">
+          Items can be added in the list by using the <code>append()</code>{' '}
+          method.
+        </Typography>
+
+        <CodeAppendExample />
+
+        {/* len() */}
+        <Typography mt="2rem" mb="1rem">
+          You can get the size of the list (how many items in the list) by using
+          the <code>len()</code> method.
+        </Typography>
+
+        <CodeLenExample />
+
+        {/* List Operations */}
+        <Typography mt="2rem" mb="1rem">
+          Lists also support various operations such as concatenation,
+          repetition, and membership testing using <code>+</code>,{' '}
+          <code>*</code>, and <code>in</code> respectively. Examples are given
+          below:
+        </Typography>
+
+        <CodeListOps />
       </Box>
     </Container>
   );
@@ -82,6 +113,102 @@ print(fruits[0]) # Output: apple`;
 
   return (
     <Box sx={{ height: '100px' }}>
+      <Editor
+        theme="vs-dark"
+        defaultLanguage="python"
+        value={code}
+        options={{
+          fontSize: '20px',
+          readOnly: true,
+          scrollBeyondLastLine: false,
+        }}
+      />
+    </Box>
+  );
+}
+
+function CodeMutableExample() {
+  const code = `fruits = ["apple", "banana", "cherry"]
+fruits[1] = "orange"
+print(fruits) # Output: ['apple', 'orange', 'cherry']`;
+
+  return (
+    <Box sx={{ height: '140px' }}>
+      <Editor
+        theme="vs-dark"
+        defaultLanguage="python"
+        value={code}
+        options={{
+          fontSize: '20px',
+          readOnly: true,
+          scrollBeyondLastLine: false,
+        }}
+      />
+    </Box>
+  );
+}
+
+function CodeListOps() {
+  const code = `fruits = ["apple", "banana"]
+
+print(fruits + ["cherry", "orange"])  # merges the 2 lists
+# Output: ['apple', 'banana', 'cherry', 'orange']
+
+print(fruits * 3) # multiplies the items in the list n times
+# Output: ['apple', 'banana', 'apple', 'banana', 'apple', 'banana']
+
+print("apple" in fruits)  # checks if the element "apple" is in the fruits list
+# Output: True`;
+
+  return (
+    <Box sx={{ height: '320px' }}>
+      <Editor
+        theme="vs-dark"
+        defaultLanguage="python"
+        value={code}
+        options={{
+          fontSize: '20px',
+          readOnly: true,
+          scrollBeyondLastLine: false,
+        }}
+      />
+    </Box>
+  );
+}
+
+function CodeAppendExample() {
+  const code = `fruits = ["apple", "banana"]
+
+fruits.append("grapes")
+print(fruits) # Output: ['apple', 'banana', 'grapes']
+`;
+
+  return (
+    <Box sx={{ height: '180px' }}>
+      <Editor
+        theme="vs-dark"
+        defaultLanguage="python"
+        value={code}
+        options={{
+          fontSize: '20px',
+          readOnly: true,
+          scrollBeyondLastLine: false,
+        }}
+      />
+    </Box>
+  );
+}
+
+function CodeLenExample() {
+  const code = `fruits = ["apple", "banana"]
+print(len(fruits)) # Output: 2
+
+fruits.append("grapes")
+print(len(fruits)) # Output: 3
+`;
+
+  return (
+    <Box sx={{ height: '200px' }}>
       <Editor
         theme="vs-dark"
         defaultLanguage="python"
